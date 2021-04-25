c0592607f46e1df83dda9887e388dac0f51c3cac
a k <rdyto1@MacBook-Pro.local>
2021-04-25 13:20:14 +0200

    step_4

--- app/routes.py
+++ app/routes.py
@@ -22,3 +22,31 @@ def html():
 def template():
     user = {'username': 'bear'}
     return render_template('template.html', title='Home', user=user)
+
+@app.route('/if1')
+def if1():
+    user = {'username': 'the user'}
+    return render_template('if.html', user=user)
+
+@app.route('/if2')
+def if2():
+    return render_template('if.html', user=None)
+
+@app.route('/for')
+def for_route():
+    user = {'username': 'FreeIt user'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Ипполит'},
+            'body': 'Какая гадость эта ваша заливная рыба!!'
+        }
+    ]
+    return render_template('for.html', title='Home', user=user, posts=posts)
