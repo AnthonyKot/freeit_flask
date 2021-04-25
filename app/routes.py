84a3368f0ddec3cfe13c0bec6caf12c37237e5fb
a k <rdyto1@MacBook-Pro.local>
2021-04-25 13:08:00 +0200

    step_3

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,24 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
-@app.route('/index')
 def index():
     return "Hello, World!"
+
+@app.route('/html')
+def html():
+    user = {'username': 'Медвед'}
+    return '''
+        <html>
+            <head>
+                <title>Home Page - Microblog</title>
+            </head>
+            <body>
+                <h1>Привет, ''' + user['username'] + '''!</h1>
+            </body>
+        </html>'''
+
+@app.route('/template')
+def template():
+    user = {'username': 'bear'}
+    return render_template('template.html', title='Home', user=user)
