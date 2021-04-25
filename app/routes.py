a93d218f91ac5e1b84fcc612e6897bfa659b1652
a k <rdyto1@MacBook-Pro.local>
2021-04-25 13:29:53 +0200

    step_5

--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,7 @@ from flask import render_template
 def index():
     return "Hello, World!"
 
+
 @app.route('/html')
 def html():
     user = {'username': 'Медвед'}
@@ -18,19 +19,23 @@ def html():
             </body>
         </html>'''
 
+
 @app.route('/template')
 def template():
     user = {'username': 'bear'}
     return render_template('template.html', title='Home', user=user)
+
 
 @app.route('/if1')
 def if1():
     user = {'username': 'the user'}
     return render_template('if.html', user=user)
 
+
 @app.route('/if2')
 def if2():
     return render_template('if.html', user=None)
+
 
 @app.route('/for')
 def for_route():
@@ -50,3 +55,19 @@ def for_route():
         }
     ]
     return render_template('for.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/child')
+def children():
+    user = {'username': 'FreeIt user'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('child.html', title='Children', user=user, posts=posts)
