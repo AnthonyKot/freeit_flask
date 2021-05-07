48f49a0741822cbf361c7ea4693e0fcd6629e81a
a k <rdyto1@MacBook-Pro.local>
2021-05-07 14:36:40 +0200

    extended profile

--- app/models.py
+++ app/models.py
@@ -9,6 +9,9 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
+    role = db.Column(db.SmallInteger, default = 0)
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
--- app/routes.py
+++ app/routes.py
@@ -4,7 +4,7 @@ from werkzeug.urls import url_parse
 from app import app, db
 from app.forms import LoginForm, RegistrationForm
 from app.models import User, Post
-
+from datetime import datetime
 
 @app.route('/')
 @app.route('/index')
@@ -64,3 +64,10 @@ def user(username):
     return render_template('user.html',
         user = user,
         posts = posts)
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.add(current_user)
+        db.session.commit()
