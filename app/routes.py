fa0f30985903b791852573f601537057cce29b73
a k <rdyto1@MacBook-Pro.local>
2021-05-07 14:10:22 +0200

    add_profile_page

--- app/routes.py
+++ app/routes.py
@@ -51,3 +51,16 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username = username).first()
+    if user == None:
+        flash('User ' + username + ' not found.')
+        return redirect(url_for('index'))
+    posts = Post.query.join(User).filter_by(username = username)
+    return render_template('user.html',
+        user = user,
+        posts = posts)
